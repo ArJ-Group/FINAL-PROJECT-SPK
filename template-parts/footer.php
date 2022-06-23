f0b01779a660bebd67814e678d7cc570dc63c4de
Rajendra Rakha <1941720080@student.polinema.ac.id>
2022-06-23 20:14:43 +0700

    Header & Footer

---
+++ template-parts/footer.php
@@ -0,0 +1,30 @@
+	</div><!-- #main -->
+	</div><!-- #main -->
+	</div><!-- #main -->
+
+
+
+	<footer class="footer-section theme-footer">
+		<div class="footer-section-2 container-fluid">
+			<div class="">
+				<ul style="text-align:center;">
+					<li class="list-inline-item  mr-3">
+						<p class="bottom-footer">&#xA9; 2022 <a target="_blank">Rajendra & Ariono</a></p>
+					</li>
+
+				</ul>
+			</div>
+
+		</div>
+	</footer>
+
+	</div><!-- #page -->
+	</body>
+
+	</html>
+	<?php
+	if (isset($pdo)) {
+		// Tutup Koneksi
+		$pdo = null;
+	}
+	?>
